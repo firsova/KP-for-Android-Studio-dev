d5d1583169f7ba5607c8ef99a370390fbaeff189
firsova <firsova-petrsu@mail.ru>
2016-10-30 18:13:03 +0300

    EDITED. DrawerBuilder() <<but "Exit" doesn't work>>

--- app/src/main/java/petrsu/smartroom/android/srcli/MicActivity.java
+++ app/src/main/java/petrsu/smartroom/android/srcli/MicActivity.java
@@ -57,57 +57,16 @@ public class MicActivity extends ActionBarActivity implements View.OnTouchListen
                 .withHeader(R.layout.drawer_header)
                 .withDrawerWidthDp(320)
                 .addDrawerItems(
-                        new SectionDrawerItem().withName(R.string.services),
-                        new PrimaryDrawerItem().withName(R.string.agenda).withIcon(FontAwesome.Icon.faw_server),
-                        new PrimaryDrawerItem().withName(R.string.presentation).withIcon(FontAwesome.Icon.faw_image),
-                        new PrimaryDrawerItem().withName("SocialProgram").withIcon(FontAwesome.Icon.faw_globe),
-
-                        new SectionDrawerItem().withName(R.string.discussion),
-                        new PrimaryDrawerItem().withName(R.string.discussionCur).withIcon(FontAwesome.Icon.faw_comment_o),
-                        new PrimaryDrawerItem().withName(R.string.discussionList).withIcon(FontAwesome.Icon.faw_comments_o),
-
-                        new SectionDrawerItem().withName(R.string.action_settings),
-                        new PrimaryDrawerItem().withName(R.string.action_settings).withIcon(FontAwesome.Icon.faw_cog),
-
-                        new SectionDrawerItem().withName(R.string.help),
-                        new PrimaryDrawerItem().withName(R.string.manual).withIcon(FontAwesome.Icon.faw_download),
-
                         new DividerDrawerItem(),
-                        new SecondaryDrawerItem().withName(R.string.exitClientTitle).withIcon(FontAwesome.Icon.faw_close),
-                        new PrimaryDrawerItem().withName("QueueActivity").withIcon(FontAwesome.Icon.faw_globe)
+                        new SecondaryDrawerItem().withName(R.string.exitClientTitle).withIcon(FontAwesome.Icon.faw_close)
                 ).withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
             @Override
             public boolean onItemClick(AdapterView<?> parent, View view, int position, long id, IDrawerItem drawerItem) {
                 //Toast.makeText(Agenda.this, String.valueOf(id), Toast.LENGTH_SHORT).show();
                 switch ((int) id) {
-                    case 1:
-                        startActivity(Navigation.getAgendaIntent(getApplicationContext()));
-                        break;
-                    case 2:
-                        startActivity(Navigation.getPresentationIntent(getApplicationContext()));
-                        break;
-                    case 3:
-                        startActivity(Navigation.getSocialProgramIntent(getApplicationContext()));
-                        break;
-                    case 5:
-                        startActivity(Navigation.getCurDisqIntent(getApplicationContext()));
-                        break;
-                    case 6:
-                        startActivity(Navigation.getDisqListIntent(getApplicationContext()));
-                        break;
-                    case 8:
-                        startActivity(Navigation.getSettingsIntent(getApplicationContext()));
-                        break;
-                    case 10:
-                        startActivity(Navigation.getManIntent(getApplicationContext()));
-                        break;
-                    case 12:
-                        startActivity(Navigation.exitApp());
-                        break;
-                    case 13:
-                        break;
-                    default:
-                        break;
+                    case 4:
+                        startActivity(Navigation.exitApp());                                        break;
+                    default:                                                                        break;
                 }
                 return true;
             }
