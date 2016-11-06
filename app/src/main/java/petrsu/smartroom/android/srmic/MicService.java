4133e2f0efd68a6141723342c6cb6a70cc2d3e70
firsova <firsova-petrsu@mail.ru>
2016-11-06 22:32:18 +0300

    ADDED. old version of Mic Service

--- app/src/main/java/petrsu/smartroom/android/srmic/MicService.java
+++ app/src/main/java/petrsu/smartroom/android/srmic/MicService.java
@@ -18,111 +18,90 @@ import java.io.IOException;
 import org.xiph.speex.SpeexEncoder;
 import android.util.Log;
 
-
 /**
- * 
+ *
  * @author pavlin
  *
- *	Service allows to interact with microphone
- *	service and use smartphone as a mike
+ * Service allows to interact with microphone
+ * service and use smartphone as a mike
  */
 public class MicService extends Service {
-	
-	private static String ip;					// Mike service IP
-	private static String port;					// Mike service port
+
+	private static String ip;              // Mike service IP
+	private static String port;                // Mike service port
 	private AudioRecord mInputData;
 	private int mMinBufferSize;
-	private static boolean mActiveMic = false;	// Service activity indicator
+	private static boolean mActiveMic = false; // Service activity indicator
 	private final int mSampleRate = 11025;
 	private final int channels = 1;
 	private final int mode = 0;
 	private final int samples = 320;
 	private final int quality = 5;
 	private SpeexEncoder encoder;
-	//private PointerByReference penc;
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
 	}
-	
+
 	@Override
 	public void onStart(Intent intent, int startId) {
-		
+
 		ip = KP.getMicServiceIP();
 		port = KP.getMicServicePort();
-		
-		/* If service is not available */
+
+      /* If service is not available */
 		if((ip == null) || (port == null)) {
-			Toast.makeText(this, R.string.mic_service_not_avail, 
+			Toast.makeText(this, R.string.mic_service_not_avail,
 					Toast.LENGTH_SHORT)
 					.show();
-			
-			/* Broadcast `stop` mike service status */
+
+         /* Broadcast `stop` mike service status */
 			Intent recvIntent = new Intent(Projector.BROADCAST_STATUS_SERVICE);
 			recvIntent.putExtra(Projector.SERVICE_STATUS, false);
 			sendBroadcast(recvIntent);
-			
+
 			stopSelf();
 			return;
 		}
-		
+
 		startRecording();
 	}
-	
+
 	/**
 	 * Initialize recorder and start sending
 	 * audio data
 	 */
-	public void startRecording() {		
+	public void startRecording() {
 		new Thread() {
 			@Override
 			public void run() {
 				initEncoder();
 				initRecorder();
-				
+
 				mInputData.startRecording();
 				mActiveMic = true;
-				
-				/* While mike is active */
+
+            /* While mike is active */
 				while(mActiveMic) {
 					byte[] audioData = new byte[samples];
-					
-					/* Read data from smartphone mike */
-					int numBytesRead = mInputData.read(audioData, 0, samples);
 
-					/*ShortBuffer shortBuffer = ShortBuffer.allocate(1024 * 1024);//откуда кодирует
-					for (int i = 0; i < numBytesRead; i += 2) {
-						int b1 = audioData[i + 1] & 0xff;
-						int b2 = audioData[i] << 8;
-						shortBuffer.put((short) (b1 | b2));
-					}
+               /* Read data from smartphone mike */
+					mInputData.read(audioData, 0, samples);
 
-          			// Encode data with opus
-					ByteBuffer dataBuffer = ByteBuffer.allocate(1024);//куда кодирует
-					int toRead = Math.min(shortBuffer.remaining(), dataBuffer.remaining());
-					int read = Opus.INSTANCE.opus_encode(penc, shortBuffer, 80, dataBuffer, toRead);
-					//dataBuffer.position(dataBuffer.position() + read);
-					//dataBuffer.flip();
-					//shortBuffer.flip();
-					byte[] encData = new byte[dataBuffer.remaining()];
-					dataBuffer.get(encData);*/
-
-					/* Encode data with Speex */
+               /* Encode data with Speex */
 					if(!encoder.processData(audioData, 0, samples))
 						Log.i("encoding", "failed");
 
 					int encDataLen = encoder.getProcessedDataByteSize();
 					byte[] encData = new byte[encDataLen];
 
-					/* Load processed data to byte array */
+               /* Load processed data to byte array */
 					encoder.getProcessedData(encData, 0);
 
-					/*Log.i("encoded data size", String.valueOf(
-						encoder.getProcessedData(encData, 0)));*/
+					Log.i("encoded data size", String.valueOf(
+							encoder.getProcessedData(encData, 0)));
 
-					sendEncodedBytes(encData);
-					
 					sendEncodedBytes(encData);
 				}
 				mActiveMic = false;
@@ -130,12 +109,12 @@ public class MicService extends Service {
 			}
 		}.start();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		mActiveMic = false;
 	}
-	
+
 	/**
 	 * Stops recording and closes
 	 * input streams
@@ -148,13 +127,11 @@ public class MicService extends Service {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Initializes Speex encoder
 	 */
 	public void initEncoder() {
-		//IntBuffer error = IntBuffer.allocate(4);
-		//penc = Opus.INSTANCE.opus_encoder_create(8000, channels, Opus.OPUS_APPLICATION_RESTRICTED_LOWDELAY, error);
 		encoder = new SpeexEncoder();
 		encoder.init(mode, quality, mSampleRate, channels);
 	}
@@ -163,28 +140,28 @@ public class MicService extends Service {
 	 * Initializes audio recorder
 	 */
 	public void initRecorder() {
-		mMinBufferSize = AudioRecord.getMinBufferSize(mSampleRate, 
+		mMinBufferSize = AudioRecord.getMinBufferSize(mSampleRate,
 				AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-		
-		mInputData = new AudioRecord(AudioSource.MIC, mSampleRate, 
-				AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, 
+
+		mInputData = new AudioRecord(AudioSource.MIC, mSampleRate,
+				AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT,
 				mMinBufferSize);
 	}
-	
+
 	/**
 	 * Sends processed data to microphone service
-	 * 
+	 *
 	 * @param data - encoded audio data
 	 */
 	public void sendEncodedBytes(final byte[] data) {
 		new Thread() {
 			@Override
 			public void run() {
-				
+
 				try {
 					DatagramSocket socket = new DatagramSocket();
-					DatagramPacket packet = new DatagramPacket(data, 
-							data.length, 
+					DatagramPacket packet = new DatagramPacket(data,
+							data.length,
 							new InetSocketAddress(ip, Integer.parseInt(port)));
 					socket.send(packet);
 					socket.close();
@@ -196,4 +173,3 @@ public class MicService extends Service {
 			}
 		}.start();
 	}
-}
